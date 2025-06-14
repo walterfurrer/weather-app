b54187b4b16aa7dce4ef462664f6573e488a5598
Walter Furrer <walterfurrer@proton.me>
2025-06-13 23:02:58 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const weatherForm = document.querySelector(".weather-form");
+const cityInput = document.querySelector(".city-input");
+const card = document.querySelector(".card");
+const apiKey = "4e9294f02613bc6d47178ee8459dccb7";
+
+weatherForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const city = cityInput.value;
+
+  if (city) {
+    try {
+      const weatherData = await getWeatherData(city);
+      displayWeatherInfo(weatherData);
+    } catch (error) {
+      console.error(error);
+      displayError(error);
+    }
+  } else {
+    displayError("Please enter a valid city");
+  }
+});
+
+async function getWeatherData(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  const response = await fetch(apiUrl);
+
+  if (!response.ok) {
+    throw new Error("Could not fetch weather data");
+  }
+  return await response.json();
+}
+
+function displayWeatherInfo(data) {
+  console.log(data);
+  const {
+    name: city,
+    main: { temp, humidity },
+    weather: [{ description, id }],
+  } = data;
+
+  card.textContent = "";
+  card.style.display = "flex";
+
+  const cityDisplay = document.createElement("h1");
+  const tempDisplay = document.createElement("p");
+  const humidityDisplay = document.createElement("p");
+  const descDisplay = document.createElement("p");
+  const weatherEmoji = document.createElement("p");
+
+  cityDisplay.textContent = city;
+  cityDisplay.classList.add("city-display");
+}
+
+function getWeatherEmoji(weatherId) {}
+
+function displayError(message) {
+  const errorDisplay = document.createElement("p");
+  errorDisplay.textContent = message;
+  errorDisplay.classList.add("error-display");
+
+  card.textContent = "";
+  card.style.display = "flex";
+  card.appendChild(errorDisplay);
+}
